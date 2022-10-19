822c2e46f08c2ae72d99d58eef76a1cd145f5df7
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-19 20:10:50 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+// import { useQuery } from '@apollo/client';
+// import gql from 'graphql-tag';
+
+// const QUERY = gql`
+//   query {
+//     allUsers {
+//       id
+//     }
+//   }
+// `;
+
+export default function HomePage() {
+  //   const { data, error, loading } = useQuery(QUERY);
+  //   console.log(data);
+  return (
+    <div>
+      <h1>Home page</h1>
+    </div>
+  );
+}
