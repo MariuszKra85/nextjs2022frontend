a6c244d1fbba58df8c785af55d72d1732ee206cf
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-19 21:21:29 +0100

    change apollo client to 3.4.11 debug __dev__ issue

--- pages/index.js
+++ pages/index.js
@@ -19,3 +19,9 @@ export default function HomePage() {
     </div>
   );
 }
+
+export async function getServerSideProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
