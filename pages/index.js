8d4fabc06d78e626ff9f8be1af53609b68befd6e
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-19 21:00:53 +0100

    add query to index

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
-// import { useQuery } from '@apollo/client';
-// import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 
-// const QUERY = gql`
-//   query {
-//     allUsers {
-//       id
-//     }
-//   }
-// `;
+const QUERY = gql`
+  query {
+    allUsers {
+      id
+      name
+    }
+  }
+`;
 
 export default function HomePage() {
-  //   const { data, error, loading } = useQuery(QUERY);
-  //   console.log(data);
+  const { data, error, loading } = useQuery(QUERY);
+  console.log(data);
   return (
     <div>
       <h1>Home page</h1>
