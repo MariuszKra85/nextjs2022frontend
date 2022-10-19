586f92b968982eb95a4993ec08d6c6aae770a3d3
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-19 21:56:31 +0100

    Add Component User remove query from index

---
+++ components/User.js
@@ -0,0 +1,22 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const QUERY = gql`
+query{
+    allUsers{
+        id
+        name
+    }
+};
+`;
+
+export default function User() {
+  const { data, error, loading } = useQuery(QUERY);
+
+  return (
+    <div>
+      <h2>Name</h2>
+      <p>user</p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
 import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
-
-const QUERY = gql`
-  query {
-    allUsers {
-      id
-      name
-    }
-  }
-`;
+import User from '../components/User';
 
 export default function HomePage() {
   const { data, error, loading } = useQuery(QUERY);
@@ -16,6 +7,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Home page</h1>
+      <User />
     </div>
   );
 }
