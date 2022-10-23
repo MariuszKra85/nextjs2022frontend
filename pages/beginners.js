a50ca7de43e3a81f06b499cc1e2579922d74a6c0
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-23 18:09:11 +0100

    add videos drome google drive and add query videos

--- pages/beginners.js
+++ pages/beginners.js
@@ -1,12 +1,67 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import React from 'react';
 import ReactPlayer from 'react-player';
+import { Player } from 'video-react';
+import styled from 'styled-components';
 import LevelButton from '../components/LevelButton';
+import { useUser } from '../lib/useUser';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const PlayerWrapper = styled.div`
+  width: 80vw;
+  max-width: 500px;
+  margin-top: 10px;
+`;
+
+const QUERY_BEGGINER_VIDEO = gql`
+  query {
+    allVideos {
+      name
+      level
+      url
+      description
+    }
+  }
+`;
+const Frame = styled.iframe`
+  border: none;
+`;
 
 export default function Beginners() {
-  return (
-    <div>
-      <p>Here will be video for beginners!!!</p>
-      <ReactPlayer url="https://youtu.be/Y_0tB71g6-A" width="80vw" />
-      <LevelButton level="Back" href="/" />
-    </div>
-  );
+  const { data: data2, error, loading } = useQuery(QUERY_BEGGINER_VIDEO);
+  const { data } = useUser();
+  console.log(data);
+  console.log(data2);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!loading) {
+    return (
+      <>
+        <p>Here will be video for beginners!!!</p>
+        {data2.allVideos.map((e) => (
+          <Wrapper key={e.name}>
+            <p>{e.name}</p>
+            <Frame
+              src="https://drive.google.com/file/d/1oq5VwNFgXWyCJWI-V1YbFEYrBbsYnIxe/preview"
+              width="340"
+              height="280"
+              allow="autoplay"
+              allowFullScreen
+            />
+            <p>{e.description}</p>
+          </Wrapper>
+        ))}
+        <LevelButton level="Back" href="/" />
+      </>
+    );
+  }
 }
