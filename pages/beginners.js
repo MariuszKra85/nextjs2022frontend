4685c34c7967a25309254198392ee3d959a1f6ad
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-23 12:08:23 +0100

    add width to player

--- pages/beginners.js
+++ pages/beginners.js
@@ -5,7 +5,7 @@ export default function Beginners() {
   return (
     <div>
       <p>Here will be video for beginners!!!</p>
-      <ReactPlayer url="https://youtu.be/Y_0tB71g6-A" />
+      <ReactPlayer url="https://youtu.be/Y_0tB71g6-A" width="80vw" />
       <LevelButton level="Back" href="/" />
     </div>
   );
