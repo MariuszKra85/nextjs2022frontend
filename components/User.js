334758020fe5eaea1b1a291ace41f8cf30a694c8
Mariusz <przyslony@gmail>
2022-12-16 17:10:12 +0000

    change to levelpage views in props send data to filter videos on site

--- components/User.js
+++ components/User.js
@@ -1,27 +1,30 @@
 import LevelButton from './LevelButton';
 
 export default function User({ user }) {
-  console.log(user);
   return (
     <div>
       <h2> Hello {user.name} nice to see you in our app!</h2>
       <p>you are on {user.role} level, you unlock video on levels:</p>
       <h2>Salsa:</h2>
-      <LevelButton level="Novice" href="/beginners" />
+      <LevelButton
+        level="Novice"
+        vidlvl="4"
+        href="/levelPage?lvl=1&type=salsa"
+      />
       {user.SalsaLevel > 1 ? (
-        <LevelButton level="Beginner" href="/improvers" />
+        <LevelButton level="Beginner" href="/levelPage?lvl=2&type=salsa" />
       ) : null}
       {user.SalsaLevel > 2 ? (
-        <LevelButton level="Improvers" href="/beginners" />
+        <LevelButton level="Improvers" href="/levelPage?lvl=3&type=salsa" />
       ) : null}
       {user.SalsaLevel > 3 && (
-        <LevelButton level="Intermediate" href="/beginners" />
+        <LevelButton level="Intermediate" href="/levelPage?lvl=4&type=salsa" />
       )}
       {user.SalsaLevel > 4 && (
-        <LevelButton level="Advanced" href="/beginners" />
+        <LevelButton level="Advanced" href="/levelPage?lvl=5&type=salsa" />
       )}
       {user.SalsaLevel > 5 && (
-        <LevelButton level="Professional" href="/beginners" />
+        <LevelButton level="Professional" href="/levelPage?lvl=6&type=salsa" />
       )}
       <h2>Rumba:</h2>
       <LevelButton level="Novice" href="/beginners" />
@@ -35,7 +38,7 @@ export default function User({ user }) {
         <LevelButton level="Intermediate" href="/beginners" />
       )}
       {user.RumbaLevel > 4 && (
-        <LevelButton level="Advanced" href="/beginners" />
+        <LevelButton level="Advanced" href="/intermidiate" />
       )}
       {user.RumbaLevel > 5 && (
         <LevelButton level="Professional" href="/beginners" />
